bd1d447963d2c83078e506af805cefe0744e5a74
Evgenij P <prishchepovee@gmail.com>
2024-05-09 20:29:42 +0300

    add publication data + 1day

--- src/videos/utils/createVideo.ts
+++ src/videos/utils/createVideo.ts
@@ -1,14 +1,19 @@
 import { createVideoModel } from "../../models/videos-models/CreateVideoModel";
 
 export const createVideo = (video: createVideoModel) => {
+  const createAt = new Date();
+
+  const publicationDate = (createAt: Date) =>
+    new Date(new Date().setDate(createAt.getDate() + 1)).toISOString();
+
   return {
     id: Date.now() + Math.random(),
     title: video.title,
     author: video.author,
     canBeDownloaded: true,
     minAgeRestriction: null,
-    createdAt: new Date().toISOString(),
-    publicationDate: new Date().toISOString(),
+    createdAt: createAt.toISOString(),
+    publicationDate: publicationDate(createAt),
     availableResolutions: video.availableResolutions
       ? video.availableResolutions
       : [],
