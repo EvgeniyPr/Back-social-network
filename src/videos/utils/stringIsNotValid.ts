4754d7e86378b38f1960379a3c4d47a5eacc86eb
Evgenij P <prishchepovee@gmail.com>
2024-05-09 23:07:51 +0300

    fix bug

--- src/videos/utils/stringIsNotValid.ts
+++ src/videos/utils/stringIsNotValid.ts
@@ -1,8 +1,8 @@
 export const stringIsNotValid = (
   stringLength: number,
-  string: string | undefined
+  string: string | undefined | null
 ): boolean => {
-  if (string === undefined) {
+  if (string === undefined || string === null) {
     return true;
   }
   if (string.trim().length > stringLength || string.trim().length === 0)
