08e4815eadaa3df8447a9dcc780999e7369d5d55
Evgenij P <prishchepovee@gmail.com>
2024-05-17 11:20:28 +0300

    add blogs repository

--- src/blogs/controllers/updateBlogController.ts
+++ src/blogs/controllers/updateBlogController.ts
@@ -5,25 +5,22 @@ import {
 } from "../../models/RequestsModels";
 import { BlogInputModel } from "../models/BlogInputModel";
 import { GetBlogByURIParamsModel } from "../models/GetBlogByURIParamsModel";
-import { findIndex } from "../../utils/findIndex";
-import { db } from "../../db/db";
 import { HTTP_STATUSES } from "../../settings/HTTP_STATUSES/HTTP_STATUSES";
+import { blogsRepository } from "../blogRepository";
 
-export const updateBlogController = (
+export const updateBlogController = async (
   req: RequestWithParams<GetBlogByURIParamsModel> &
     RequestWithBody<BlogInputModel>,
   res: Response
 ) => {
   //validation middleware first
-  const indexBlogToUpdate = findIndex(req.params.id, db.blogs);
-  if (indexBlogToUpdate > -1) {
-    db.blogs[indexBlogToUpdate] = {
-      ...db.blogs[indexBlogToUpdate],
-      ...req.body,
-    };
+  const isUpdate = await blogsRepository.updateBlogController(
+    req.params.id,
+    req.body
+  );
+  if (isUpdate) {
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
     return;
-  } else {
-    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
   }
+  res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
 };
