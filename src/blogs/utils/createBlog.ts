d5fece4053b0821ab7ce692a15be7aeba60e68aa
Evgenij P <prishchepovee@gmail.com>
2024-05-20 00:38:09 +0300

    Code Refactoring

--- src/blogs/utils/createBlog.ts
+++
@@ -1,6 +0,0 @@
-import { BlogInputModel } from "../models/BlogInputModel";
-import { BlogOutputModel } from "../models/BlogOutputModel";
-
-export const createBlog = (data: BlogInputModel): BlogOutputModel => {
-  return { ...data, id: (Date.now() + Math.random()).toString() };
-};
