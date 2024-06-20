2988f7bfc3daf3b59dc3b4c1bd740dc5abced385
Evgenij P <prishchepovee@gmail.com>
2024-06-20 14:01:41 +0300

    craete postsMongoDbRepository

--- src/posts/controllers/getPostController.ts
+++ src/posts/controllers/getPostController.ts
@@ -3,7 +3,10 @@ import { RequestWithParams } from "../../models/RequestsModels";
 import { GetPostByURIParamsModel } from "../models/GetPostByURIParamsModel";
 import { PostOutputModelToFront } from "../models/PostOutputModel";
 import { postRepository } from "../repositories/postsRepository";
+import { PostOutputModelToFront } from "../models/PostOutputModel";
+import { postRepository } from "../repositories/postsRepository";
 import { HTTP_STATUSES } from "../../settings/HTTP_STATUSES/HTTP_STATUSES";
+import { postsMongoDbRepository } from "../repositories/postsMongoDbRepository";
 import { postsMongoDbRepository } from "../repositories/postsMongoDbRepository";
 
 export const getPostsController = async (
@@ -12,9 +15,12 @@ export const getPostsController = async (
 ) => {
   if (!req.params.id) {
     const posts = await postsMongoDbRepository.getPosts();
+    console.log("posts", posts);
+
     res.status(HTTP_STATUSES.OK_200).json(posts);
     return;
   } else {
+    const post = await postsMongoDbRepository.getPost(req.params.id);
     const post = await postsMongoDbRepository.getPost(req.params.id);
     if (post) {
       res.status(HTTP_STATUSES.OK_200).json(post);
