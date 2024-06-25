687a6e60010b35982630dd25cdf1af309a434c56
Evgenij P <prishchepovee@gmail.com>
2024-06-25 10:06:57 +0300

    del commits with uri

--- src/posts/repositories/postsMongoDbRepository.ts
+++ src/posts/repositories/postsMongoDbRepository.ts
@@ -3,7 +3,7 @@ import { postCollection } from "../../db/mongo-db";
 import {
   PostOutputModelToFront,
   PostsOutputModelFromDb,
-} from "../models/PostOutputModelToFront";
+} from "../models/PostOutputModel";
 import { PostInputModel } from "../models/PostInputModel";
 import { blogsMongoDBRepository } from "../../blogs/repositories/blogsMongoDbRepository";
 import { errors } from "../../errors/errors";
