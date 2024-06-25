c7a4b46f50bfd8496412af5da2524f1b506f15b8
Evgenij P <prishchepovee@gmail.com>
2024-06-25 11:26:17 +0300

    add favicon

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { SETTINGS } from "./settings/settings";
 import { videoRouter } from "./routers/videoRouter";
 import { db } from "./db/db";
+import favicon from "serve-favicon";
 import { HTTP_STATUSES } from "./settings/HTTP_STATUSES/HTTP_STATUSES";
 import { blogsRouter } from "./routers/blogsRouter";
 import { postsRouter } from "./routers/postsRouter";
@@ -11,6 +13,8 @@ export const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(SETTINGS.PASS.VIDEO, videoRouter);
 app.use(SETTINGS.PASS.BLOGS, blogsRouter);
 app.use(SETTINGS.PASS.POSTS, postsRouter);
